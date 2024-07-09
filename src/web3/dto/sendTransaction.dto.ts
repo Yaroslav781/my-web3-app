040e1a39bbf2ac062aebc4f80d9ab0583f6c64c3
Yaroslav781 <pelexach12@gmail.com>
2024-07-09 14:39:06 +0300

    created send transaction dto

---
+++ src/web3/dto/sendTransaction.dto.ts
@@ -0,0 +1,30 @@
+import {
+  IsEthereumAddress,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Min,
+} from 'class-validator';
+
+export class SendTransactionDto {
+  @IsNotEmpty()
+  @IsEthereumAddress()
+  token_addr: string;
+
+  @IsNotEmpty()
+  @IsEthereumAddress()
+  user_addr: string;
+
+  @IsNotEmpty()
+  @IsEthereumAddress()
+  recipient_addr: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
+  amount: number;
+
+  @IsNotEmpty()
+  @IsString()
+  privateKey: string;
+}
