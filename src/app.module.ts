f1b6c0f85355ac7304f6b2b0e8e392ee308ff38b
Yaroslav781 <pelexach12@gmail.com>
2024-07-09 19:36:58 +0300

    filled app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
+import { Web3Module } from './web3/web3.module';
 
 @Module({
-  imports: [],
+  imports: [Web3Module],
   controllers: [AppController],
   providers: [AppService],
 })
